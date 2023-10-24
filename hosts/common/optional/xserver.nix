64f3699f9e9f3b31db8f4e9fbc9e9e1c1f6393ac
Philip Schömig <philip.schoemig@posteo.de>
2023-10-24 16:34:23 +0200

    feat(hosts/common/optional/xserver): enable XFCE desktop environment
    Switch from GDM to LightDM display manager.

--- hosts/common/optional/xserver.nix
+++ hosts/common/optional/xserver.nix
@@ -3,12 +3,11 @@
     xserver = {
       enable = true;
 
-      desktopManager.gnome = {
-        enable = true;
-      };
-      displayManager.gdm = {
-        enable = true;
-      };
+      desktopManager.gnome.enable = true;
+      desktopManager.xfce.enable = true;
+
+      # displayManager.gdm.enable = true;
+      displayManager.lightdm.enable = true;
 
       layout = "de";
     };
