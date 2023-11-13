bfb10293328b8985cef5b9c9dafb3b944a0d565d
Philip Schömig <philip.schoemig@posteo.de>
2023-11-13 22:03:12 +0100

    fix(home/features/desktop): avoid missing system tray error by delaying autostart

--- home/philip/features/desktop/common/activitywatch.nix
+++ home/philip/features/desktop/common/activitywatch.nix
@@ -1,4 +1,6 @@
 {pkgs, ...}: let
+  inherit (pkgs.lib.strings) fileContents;
+
   package = pkgs.activitywatch;
   application = "aw-qt";
 in {
@@ -6,8 +8,11 @@ in {
     package
   ];
 
+  # TODO: use xdg.desktopEntries (https://nix-community.github.io/home-manager/options.html#opt-xdg.desktopEntries) instead?
   xdg.configFile."${application}" = {
-    source = "${package}/share/applications/${application}.desktop";
+    text = builtins.replaceStrings ["Exec=aw-qt"] ["Exec=sh -c \"sleep 5 && aw-qt\""] (
+      fileContents "${pkgs.aw-qt}/share/applications/${application}.desktop"
+    );
     target = "autostart/${application}.desktop";
   };
 }
