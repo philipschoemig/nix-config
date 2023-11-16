117266637c4952a7ab3dd5c33ed9a0461436cd7c
Philip Schömig <philip.schoemig@posteo.de>
2023-11-16 09:47:03 +0100

    feat(hosts/secunet-thinkpad): restructure nixos configuration

--- hosts/secunet-thinkpad/default.nix
+++ hosts/secunet-thinkpad/default.nix
@@ -1,34 +1,27 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running `nixos-help`).
-
-{ config, pkgs, ... }:
-
-{
+{inputs, ...}: {
   imports = [
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-gpu-intel
+    inputs.hardware.nixosModules.common-pc-ssd
+
     ./hardware-configuration.nix
 
     ../common/global
     ../common/users/philip
 
     ../common/optional/browser.nix
+    ../common/optional/laptop-battery.nix
     ../common/optional/office.nix
     ../common/optional/pipewire.nix
+    ../common/optional/systemd-boot.nix
     ../common/optional/xserver.nix
   ];
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot = {
-    enable = true;
-    configurationLimit = 10;
-  };
-  boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.systemd.enable = true;
   boot.initrd.luks.devices = {
     "nixos-enc" = {
       device = "/dev/nvme0n1p2";
-      crypttabExtraOpts = [ "fido2-device=auto" ];
+      crypttabExtraOpts = ["fido2-device=auto"];
       # You may want to set preLVM to false if you need to start a network service first
       preLVM = true;
     };
@@ -40,70 +33,17 @@
   hardware.bluetooth.enable = true;
 
   networking.hostName = "secunet-thinkpad"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Allow unfree packages.
-  nixpkgs.config.allowUnfree = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable firmware update service.
   services.fwupd.enable = true;
 
-  services.logind.lidSwitch = "suspend";
-  services.power-profiles-daemon.enable = false;
-  services.thermald.enable = true;
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      # The following prevents the battery from charging fully to
-      # preserve lifetime. Run `tlp fullcharge` to temporarily force
-      # full charge.
-      # https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
-      START_CHARGE_THRESH_BAT0=75;
-      STOP_CHARGE_THRESH_BAT0=80;
-    };
+  services.logind = {
+    lidSwitch = "suspend";
+    lidSwitchExternalPower = "lock";
   };
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  services.thermald.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -112,6 +52,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
 }
-
