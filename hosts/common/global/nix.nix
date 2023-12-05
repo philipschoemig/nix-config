f779af05efc45f6a481f198beec9dca8927b828f
Philip Schömig <philip.schoemig@posteo.de>
2023-12-05 22:50:45 +0100

    feat(hosts/common): enable automatic nix garbage collection

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -1,10 +1,9 @@
-{ inputs, lib, ... }:
-{
+{lib, ...}: {
   nix = {
     settings = {
-      trusted-users = [ "root" "@wheel" ];
+      trusted-users = ["root" "@wheel"];
       auto-optimise-store = lib.mkDefault true;
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      experimental-features = ["nix-command" "flakes" "repl-flake"];
     };
 
     extraOptions = ''
@@ -12,11 +11,11 @@
       keep-derivations = true
     '';
 
-    # gc = {
-    #   automatic = true;
-    #   dates = "weekly";
-    #   # Keep the last 3 generations
-    #   options = "--delete-older-than +3";
-    # };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Keep the last 10 generations
+      options = "--delete-older-than +10";
+    };
   };
 }
