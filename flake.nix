835fb9385462e3329f3d84d376e24c78f60508e4
Philip Schömig <philip.schoemig@posteo.de>
2023-12-05 22:41:09 +0100

    feat: update to nixpkgs and home-manager release 23.11

--- flake.nix
+++ flake.nix
@@ -2,19 +2,14 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     hardware.url = "github:nixos/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
 
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
@@ -22,54 +17,57 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      lib = nixpkgs.lib // home-manager.lib;
-      systems = [ "x86_64-linux" ];
-      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
-      pkgsFor = lib.genAttrs systems (system: import nixpkgs {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib;
+    systems = ["x86_64-linux"];
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (system:
+      import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       });
-    in
-    {
-      inherit lib;
+  in {
+    inherit lib;
 
-      overlays = import ./overlays { inherit inputs outputs; };
+    overlays = import ./overlays {inherit inputs outputs;};
 
-      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
-      formatter = forEachSystem (pkgs: pkgs.nixpkgs-fmt);
+    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+    formatter = forEachSystem (pkgs: pkgs.nixpkgs-fmt);
 
-      nixosConfigurations = {
-        # Personal laptop
-        "lenovo-yoga" = lib.nixosSystem {
-          modules = [
-            ./hosts/lenovo-yoga
-          ];
-          specialArgs = { inherit inputs outputs; };
-        };
-        # Work laptop
-        "secunet-thinkpad" = lib.nixosSystem {
-          modules = [
-            ./hosts/secunet-thinkpad
-          ];
-          specialArgs = { inherit inputs outputs; };
-        };
+    nixosConfigurations = {
+      # Personal laptop
+      "lenovo-yoga" = lib.nixosSystem {
+        modules = [
+          ./hosts/lenovo-yoga
+        ];
+        specialArgs = {inherit inputs outputs;};
       };
-
-      homeConfigurations = {
-        "philip@lenovo-yoga" = lib.homeManagerConfiguration {
-          modules = [ ./home/philip/lenovo-yoga.nix ];
-          pkgs = pkgsFor.x86_64-linux;
-          extraSpecialArgs = { inherit inputs outputs; };
-        };
-        "philip@secunet-thinkpad" = lib.homeManagerConfiguration {
-          modules = [ ./home/philip/secunet-thinkpad.nix ];
-          pkgs = pkgsFor.x86_64-linux;
-          extraSpecialArgs = { inherit inputs outputs; };
-        };
+      # Work laptop
+      "secunet-thinkpad" = lib.nixosSystem {
+        modules = [
+          ./hosts/secunet-thinkpad
+        ];
+        specialArgs = {inherit inputs outputs;};
       };
     };
-}
 
+    homeConfigurations = {
+      "philip@lenovo-yoga" = lib.homeManagerConfiguration {
+        modules = [./home/philip/lenovo-yoga.nix];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+      "philip@secunet-thinkpad" = lib.homeManagerConfiguration {
+        modules = [./home/philip/secunet-thinkpad.nix];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+    };
+  };
+}
