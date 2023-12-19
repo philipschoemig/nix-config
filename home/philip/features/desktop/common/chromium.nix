d18150fae1899f8982ed4542564ec0e0cfa37cb6
Philip Schömig <philip.schoemig@posteo.de>
2023-12-20 00:45:47 +0100

    feat(home/features/desktop): add additional dictionaries to chromium

--- home/philip/features/desktop/common/chromium.nix
+++ home/philip/features/desktop/common/chromium.nix
@@ -2,6 +2,12 @@
   programs.chromium = {
     enable = true;
 
+    dictionaries = with pkgs.hunspellDictsChromium; [
+      en_US
+      en_GB
+      de_DE
+    ];
+
     extensions = [
       "mlomiejdfkolichcflejclcbmpeaniij" # Ghostery – Privacy Ad Blocker
       "oboonakemofpalcgghocfoadofidjkkk" # KeePassXC-Browser
