df80cb080c91d0a2a91560a378d2eb4f24cb21ac
Philip Schömig <philip.schoemig@posteo.de>
2024-01-07 21:06:13 +0100

    fix(home/features/desktop): use correct type for auto-power-on dconf setting

--- home/philip/features/desktop/common/blueman.nix
+++ home/philip/features/desktop/common/blueman.nix
@@ -1,10 +1,14 @@
-{osConfig, ...}: {
+{
+  lib,
+  osConfig,
+  ...
+}: {
   services.blueman-applet.enable = osConfig.hardware.bluetooth.enable;
 
   dconf.settings = {
     "org/blueman/plugins/powermanager" = {
       # do not automatically power on bluetooth adapter
-      auto-power-on = false;
+      auto-power-on = lib.hm.gvariant.mkJust false;
     };
   };
 }
