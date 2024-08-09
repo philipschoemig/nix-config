6d3b649952348f56e78ea6b930aec5d5b03f264d
Philip Schömig <philip.schoemig@posteo.de>
2024-08-09 09:50:03 +0200

    fix(hosts): fix duplicate declaration for option `hardware.intelgpu.driver`

--- hosts/secunet-thinkpad/default.nix
+++ hosts/secunet-thinkpad/default.nix
@@ -1,7 +1,6 @@
 {inputs, ...}: {
   imports = [
     inputs.hardware.nixosModules.common-cpu-intel
-    inputs.hardware.nixosModules.common-gpu-intel
     inputs.hardware.nixosModules.common-pc-ssd
 
     ./hardware-configuration.nix
