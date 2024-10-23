1554d6846568e3cdf8abbeb2afc1869b97abf748
Philip Schömig <philip.schoemig@posteo.de>
2024-10-23 02:06:29 +0200

    feat(home/features/desktop): set xfconf settings
    * Enable xfce4-session option "compat/LaunchGNOME" to start gnome services, like gnome-keyring
    * Set xfwm4 option "general/activate_action" to "none" to avoid bringing raised window to current workspace

--- home/philip/features/desktop/xfce/default.nix
+++ home/philip/features/desktop/xfce/default.nix
@@ -4,6 +4,15 @@
   imports = [ ../common ];
 
   home.packages = with pkgs.xfce; [
-    catfish
+    catfish # XFCE file search tool
   ];
+
+  xfconf.settings = {
+    xfce4-session = {
+      "compat/LaunchGNOME" = true;
+    };
+    xfwm4 = {
+      "general/activate_action" = "none";
+    };
+  };
 }
