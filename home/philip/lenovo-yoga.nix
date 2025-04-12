ca7e39f60f1f9a6ed2a24822afc4f04c2c3bf3ad
Philip Schömig <philip.schoemig@posteo.de>
2025-04-12 02:05:12 +0200

    refactor(home/lenovo-yoga): use absolute path for nvidia-smi in conky config
    This is a try to fix the "nvidia-smi: command not found" error in the conky log.

--- home/philip/lenovo-yoga.nix
+++ home/philip/lenovo-yoga.nix
@@ -9,17 +9,19 @@
     ./features/virtualization
   ];
 
-  conkyConfig = {
+  conkyConfig = let
+    nvidiaSmi = "/run/current-system/sw/bin/nvidia-smi";
+  in {
     enable = true;
     cpuTemp = ''''${hwmon coretemp temp 1}'';
     # gpuModel = ''''${nvidia modelname 0}'';
-    gpuModel = ''''${execi 86400 nvidia-smi --query-gpu=name --format=csv,noheader}'';
+    gpuModel = ''''${execi 86400 ${nvidiaSmi} --query-gpu=name --format=csv,noheader}'';
     # gpuFreq = ''''${nvidia gpufreq 0}'';
-    gpuFreq = ''''${exec nvidia-smi --query-gpu=clocks.gr --format=csv,noheader | cut -d' ' -f1}'';
+    gpuFreq = ''''${exec ${nvidiaSmi} --query-gpu=clocks.gr --format=csv,noheader | cut -d' ' -f1}'';
     # gpuMemFreq = ''''${nvidia memfreq 0}'';
-    gpuMemFreq = ''''${exec nvidia-smi --query-gpu=clocks.mem --format=csv,noheader | cut -d' ' -f1}'';
+    gpuMemFreq = ''''${exec ${nvidiaSmi} --query-gpu=clocks.mem --format=csv,noheader | cut -d' ' -f1}'';
     # gpuTemp = ''''${nvidia gputemp 0}'';
-    gpuTemp = ''''${exec nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader}'';
+    gpuTemp = ''''${exec ${nvidiaSmi} --query-gpu=temperature.gpu --format=csv,noheader}'';
     ioTemp = ''''${hwmon nvme temp 1}'';
     networkWirelessDevice = "wlp0s20f3";
   };
