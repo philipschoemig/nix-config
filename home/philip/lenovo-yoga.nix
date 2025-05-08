cdc1b33d9ef69d3a034a558911834ab13263ab29
Philip Schömig <philip.schoemig@posteo.de>
2025-05-09 01:46:52 +0200

    refactor(home/lenovo-yoga): remove games module for lenovo-yoga configuration

--- home/philip/lenovo-yoga.nix
+++ home/philip/lenovo-yoga.nix
@@ -3,26 +3,27 @@
     ./global
     ./features/accounts/posteo.nix
     ./features/desktop/xfce
-    ./features/games
     ./features/productivity
     ./features/social
     ./features/virtualization
   ];
 
-  conkyConfig = let
-    nvidiaSmi = "/run/current-system/sw/bin/nvidia-smi";
-  in {
-    enable = true;
-    cpuTemp = ''''${hwmon coretemp temp 1}'';
-    # gpuModel = ''''${nvidia modelname 0}'';
-    gpuModel = ''''${execi 86400 ${nvidiaSmi} --query-gpu=name --format=csv,noheader}'';
-    # gpuFreq = ''''${nvidia gpufreq 0}'';
-    gpuFreq = ''''${exec ${nvidiaSmi} --query-gpu=clocks.gr --format=csv,noheader | cut -d' ' -f1}'';
-    # gpuMemFreq = ''''${nvidia memfreq 0}'';
-    gpuMemFreq = ''''${exec ${nvidiaSmi} --query-gpu=clocks.mem --format=csv,noheader | cut -d' ' -f1}'';
-    # gpuTemp = ''''${nvidia gputemp 0}'';
-    gpuTemp = ''''${exec ${nvidiaSmi} --query-gpu=temperature.gpu --format=csv,noheader}'';
-    ioTemp = ''''${hwmon nvme temp 1}'';
-    networkWirelessDevice = "wlp0s20f3";
-  };
+  conkyConfig =
+    let
+      nvidiaSmi = "/run/current-system/sw/bin/nvidia-smi";
+    in
+    {
+      enable = true;
+      cpuTemp = ''''${hwmon coretemp temp 1}'';
+      # gpuModel = ''''${nvidia modelname 0}'';
+      gpuModel = ''''${execi 86400 ${nvidiaSmi} --query-gpu=name --format=csv,noheader}'';
+      # gpuFreq = ''''${nvidia gpufreq 0}'';
+      gpuFreq = ''''${exec ${nvidiaSmi} --query-gpu=clocks.gr --format=csv,noheader | cut -d' ' -f1}'';
+      # gpuMemFreq = ''''${nvidia memfreq 0}'';
+      gpuMemFreq = ''''${exec ${nvidiaSmi} --query-gpu=clocks.mem --format=csv,noheader | cut -d' ' -f1}'';
+      # gpuTemp = ''''${nvidia gputemp 0}'';
+      gpuTemp = ''''${exec ${nvidiaSmi} --query-gpu=temperature.gpu --format=csv,noheader}'';
+      ioTemp = ''''${hwmon nvme temp 1}'';
+      networkWirelessDevice = "wlp0s20f3";
+    };
 }
