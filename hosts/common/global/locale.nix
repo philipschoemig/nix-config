6440900c670bc46bf5068a887a338502697835eb
Philip Schömig <philip.schoemig@posteo.de>
2025-06-03 00:37:24 +0200

    refactor(hosts/common): place locale literals in variables with speaking names

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,22 +1,28 @@
-{ lib, ... }: {
+{ lib, ... }:
+
+let
+  englishLocale = "en_US.UTF-8";
+  germanLocale = "de_DE.UTF-8";
+in
+{
   i18n = {
-    defaultLocale = lib.mkDefault "en_US.UTF-8";
+    defaultLocale = lib.mkDefault englishLocale;
 
     extraLocaleSettings = {
-      LC_ADDRESS = lib.mkDefault "de_DE.UTF-8";
-      LC_IDENTIFICATION = lib.mkDefault "de_DE.UTF-8";
-      LC_MEASUREMENT = lib.mkDefault "de_DE.UTF-8";
-      LC_MONETARY = lib.mkDefault "de_DE.UTF-8";
-      LC_NAME = lib.mkDefault "de_DE.UTF-8";
-      LC_NUMERIC = lib.mkDefault "de_DE.UTF-8";
-      LC_PAPER = lib.mkDefault "de_DE.UTF-8";
-      LC_TELEPHONE = lib.mkDefault "de_DE.UTF-8";
-      LC_TIME = lib.mkDefault "de_DE.UTF-8";
+      LC_ADDRESS = lib.mkDefault germanLocale;
+      LC_IDENTIFICATION = lib.mkDefault germanLocale;
+      LC_MEASUREMENT = lib.mkDefault germanLocale;
+      LC_MONETARY = lib.mkDefault germanLocale;
+      LC_NAME = lib.mkDefault germanLocale;
+      LC_NUMERIC = lib.mkDefault germanLocale;
+      LC_PAPER = lib.mkDefault germanLocale;
+      LC_TELEPHONE = lib.mkDefault germanLocale;
+      LC_TIME = lib.mkDefault germanLocale;
     };
 
     supportedLocales = lib.mkDefault [
-      "en_US.UTF-8/UTF-8"
-      "de_DE.UTF-8/UTF-8"
+      "${englishLocale}/UTF-8"
+      "${germanLocale}/UTF-8"
     ];
   };
 
