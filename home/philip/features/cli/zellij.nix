6fc85a1ccfe89006470fa3d4d675191215fb838e
Philip Schömig <philip.schoemig@posteo.de>
2025-06-22 00:34:10 +0200

    feat(home/features/cli): set zellij variable to exit the shell automatically

--- home/philip/features/cli/zellij.nix
+++ home/philip/features/cli/zellij.nix
@@ -1,24 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.zellij = {
     enable = true;
     enableBashIntegration = false;
     enableFishIntegration = false;
-    exitShellOnExit = true;
   };
 
   programs.bash.bashrcExtra = ''
     if [[ $TERM == "alacritty" ]]; then
-      eval "$(${pkgs.zellij}/bin/zellij setup --generate-auto-start bash)"
-      eval "$(${pkgs.zellij}/bin/zellij setup --generate-completion bash)"
+      ZELLIJ_AUTO_EXIT="true"
+      eval "$(${lib.getExe pkgs.zellij} setup --generate-auto-start bash)"
+      eval "$(${lib.getExe pkgs.zellij} setup --generate-completion bash)"
     fi
   '';
 
   programs.fish.interactiveShellInit = ''
     if test "$TERM" = alacritty
-      eval (${pkgs.zellij}/bin/zellij setup --generate-auto-start fish | string collect)
-      eval (${pkgs.zellij}/bin/zellij setup --generate-completion fish | string collect)
+      set ZELLIJ_AUTO_EXIT true
+      eval (${lib.getExe pkgs.zellij} setup --generate-auto-start fish | string collect)
+      eval (${lib.getExe pkgs.zellij} setup --generate-completion fish | string collect)
     end
   '';
 }
