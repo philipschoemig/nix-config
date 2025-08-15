1b6c3f642150d5df6a9bbf43446e35e30e2b3f02
Philip Schömig <philip.schoemig@posteo.de>
2025-08-15 03:05:51 +0200

    feat(hosts): add module for openssh

--- hosts/geekom-a7/default.nix
+++ hosts/geekom-a7/default.nix
@@ -16,6 +16,7 @@
     ../common/optional/browser.nix
     ../common/optional/cryptfs.nix
     ../common/optional/graphics.nix
+    ../common/optional/openssh.nix
     ../common/optional/password-manager.nix
     ../common/optional/pipewire.nix
     ../common/optional/printing.nix
