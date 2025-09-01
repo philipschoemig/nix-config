004758870bec6eb1c9c4864c54e65c406461c647
Philip Schömig <philip.schoemig@posteo.de>
2025-09-01 08:55:45 +0200

    feat(home): enable conky config for host secunet-thinkpad

--- home/philip/secunet-thinkpad.nix
+++ home/philip/secunet-thinkpad.nix
@@ -1,5 +1,3 @@
-{ inputs, outputs, ... }:
-
 {
   imports = [
     ./global
@@ -8,4 +6,16 @@
   ];
 
   programs.git.userEmail = "philip.schoemig@secunet.com";
+
+  conkyConfig = {
+    enable = true;
+    cpuTemp = ''''${hwmon coretemp temp 1}'';
+    gpuModel = ''''${exec lspci -mm | awk -F '\"|\" \"|\\(' '/"Display|"3D|"VGA/ {print $3 " " $4}'}'';
+    gpuFreq = ''''${exec grep -Po '\d+:\s\K(\d+)(?=.*\*$)' /sys/class/drm/card*/device/pp_dpm_sclk | head -1}'';
+    gpuMemFreq = ''N/A'';
+    gpuTemp = ''N/A'';
+    ioTemp = ''''${hwmon nvme temp 1}'';
+    networkWiredDevice = "enp0s31f6";
+    networkWirelessDevice = "wlp0s20f3";
+  };
 }
