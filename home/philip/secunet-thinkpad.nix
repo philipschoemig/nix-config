4b0100946f012607ff05b1d8db09b710d7c4060e
Philip Schömig <philip.schoemig@posteo.de>
2025-10-17 10:05:55 +0200

    feat(home): add ssh client config for host secunet-thinkpad

--- home/philip/secunet-thinkpad.nix
+++ home/philip/secunet-thinkpad.nix
@@ -7,6 +7,51 @@
 
   programs.git.userEmail = "philip.schoemig@secunet.com";
 
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "git.seven.secucloud.secunet.com" = {
+        identityFile = "~/.ssh/id_ed25519";
+        identitiesOnly = true;
+      };
+      "edge" = {
+        hostname = "10.100.0.2";
+        user = "root";
+        extraOptions = {
+          "StrictHostKeyChecking" = "no";
+          "UserKnownHostsFile" = "/dev/null";
+        };
+      };
+      "test_client" = {
+        hostname = "localhost";
+        port = 2221;
+        user = "root";
+        extraOptions = {
+          "StrictHostKeyChecking" = "no";
+          "UserKnownHostsFile" = "/dev/null";
+        };
+      };
+      "test_router" = {
+        hostname = "localhost";
+        port = 2222;
+        user = "root";
+        extraOptions = {
+          "StrictHostKeyChecking" = "no";
+          "UserKnownHostsFile" = "/dev/null";
+        };
+      };
+      "test_server" = {
+        hostname = "localhost";
+        port = 2223;
+        user = "root";
+        extraOptions = {
+          "StrictHostKeyChecking" = "no";
+          "UserKnownHostsFile" = "/dev/null";
+        };
+      };
+    };
+  };
+
   conkyConfig = {
     enable = true;
     cpuTemp = ''''${hwmon coretemp temp 1}'';
