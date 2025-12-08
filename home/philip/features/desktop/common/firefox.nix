3cad30e8de44ed0818e3a17fa95e513f1573fa4f
Philip Schömig <philip.schoemig@posteo.de>
2025-12-08 23:25:19 +0100

    feat(home/features/desktop): install firefox extension bitwarden and update search engines

--- home/philip/features/desktop/common/firefox.nix
+++ home/philip/features/desktop/common/firefox.nix
@@ -1,116 +1,124 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   programs.firefox = {
     enable = true;
 
-    profiles."default" = {
+    profiles.default = {
       isDefault = true;
 
       extensions.packages = with pkgs.inputs.firefox-addons; [
+        bitwarden
         ghostery
         keepassxc-browser
         leechblock-ng
       ];
 
-      search.default = "Startpage";
-      search.force = true;
+      search = {
+        force = true;
+        default = "Startpage";
+        privateDefault = "Startpage";
 
-      search.engines = {
-        "Startpage" = {
-          urls = [ { template = "https://www.startpage.com/sp/search?query={searchTerms}"; } ];
-          icon = "https://www.startpage.com/sp/cdn/favicons/favicon-16x16--default.png";
-          definedAliases = [ "@startpage" ];
-        };
+        engines = {
+          "Startpage" = {
+            urls = [ { template = "https://www.startpage.com/sp/search?query={searchTerms}"; } ];
+            icon = "https://www.startpage.com/sp/cdn/favicons/favicon-16x16--default.png";
+            definedAliases = [ "@startpage" ];
+          };
 
-        "SearXNG" = {
-          urls = [ { template = "https://search.sapti.me/search?q={searchTerms}"; } ];
-          icon = "https://search.sapti.me/static/themes/simple/img/favicon.png";
-          definedAliases = [ "@searx" ];
-        };
+          "SearXNG" = {
+            urls = [ { template = "https://search.sapti.me/search?q={searchTerms}"; } ];
+            icon = "https://search.sapti.me/static/themes/simple/img/favicon.png";
+            definedAliases = [ "@searx" ];
+          };
 
-        "SearXNG Alternative" = {
-          urls = [ { template = "https://search.demoniak.ch/search?q={searchTerms}"; } ];
-          icon = "https://search.demoniak.ch/static/themes/simple/img/favicon.png";
-          definedAliases = [ "@searx-alt" ];
-        };
+          "SearXNG Alternative" = {
+            urls = [ { template = "https://search.demoniak.ch/search?q={searchTerms}"; } ];
+            icon = "https://search.demoniak.ch/static/themes/simple/img/favicon.png";
+            definedAliases = [ "@searx-alt" ];
+          };
 
-        "Nix API Search" = {
-          urls = [ { template = "https://noogle.dev/q?term={searchTerms}"; } ];
-          icon = "https://noogle.dev/favicon.png";
-          definedAliases = [ "@nixapi" ];
-        };
+          "Nix API Search" = {
+            urls = [ { template = "https://noogle.dev/q?term={searchTerms}"; } ];
+            icon = "https://noogle.dev/favicon.png";
+            definedAliases = [ "@nixapi" ];
+          };
 
-        "NixOS Packages" = {
-          urls = [
-            {
-              template = "https://search.nixos.org/packages";
-              params = [
-                {
-                  name = "type";
-                  value = "packages";
-                }
-                {
-                  name = "query";
-                  value = "{searchTerms}";
-                }
-              ];
-            }
-          ];
+          "NixOS Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "channel";
+                    value = lib.trivial.release;
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
 
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@nixpkgs" ];
-        };
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nixpkgs" ];
+          };
 
-        "NixOS Options" = {
-          urls = [
-            {
-              template = "https://search.nixos.org/options";
-              params = [
-                {
-                  name = "type";
-                  value = "packages";
-                }
-                {
-                  name = "query";
-                  value = "{searchTerms}";
-                }
-              ];
-            }
-          ];
+          "NixOS Options" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/options";
+                params = [
+                  {
+                    name = "channel";
+                    value = lib.trivial.release;
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
 
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@nixopts" ];
-        };
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nixopts" ];
+          };
 
-        "NixOS Wiki" = {
-          urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
-          icon = "https://wiki.nixos.org/nixos.png";
-          definedAliases = [ "@nixwiki" ];
-        };
+          "NixOS Wiki" = {
+            urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
+            icon = "https://wiki.nixos.org/nixos.png";
+            definedAliases = [ "@nixwiki" ];
+          };
 
-        "Home-Manager Options" = {
-          urls = [ { template = "https://home-manager-options.extranix.com/?query={searchTerms}"; } ];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@homemanager" ];
-        };
+          "Home-Manager Options" = {
+            urls = [ { template = "https://home-manager-options.extranix.com/?query={searchTerms}"; } ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@homemanager" ];
+          };
 
-        "Rust Crates" = {
-          urls = [ { template = "https://crates.io/search?q={searchTerms}"; } ];
-          icon = "https://crates.io/favicon.ico";
-          definedAliases = [ "@rustcrates" ];
-        };
+          "Rust Crates" = {
+            urls = [ { template = "https://crates.io/search?q={searchTerms}"; } ];
+            icon = "https://crates.io/favicon.ico";
+            definedAliases = [ "@rustcrates" ];
+          };
 
-        "Rust Docs" = {
-          urls = [ { template = "https://docs.rs/releases/search?query={searchTerms}"; } ];
-          icon = "https://docs.rs/-/rustdoc.static/favicon-32x32-422f7d1d52889060.png";
-          definedAliases = [ "@rustdocs" ];
-        };
+          "Rust Docs" = {
+            urls = [ { template = "https://docs.rs/releases/search?query={searchTerms}"; } ];
+            icon = "https://docs.rs/-/rustdoc.static/favicon-32x32-422f7d1d52889060.png";
+            definedAliases = [ "@rustdocs" ];
+          };
 
-        "dict.cc Dictionary" = {
-          urls = [ { template = "https://www.dict.cc/?s={searchTerms}"; } ];
-          icon = "https://www4.dict.cc/img/favicons/favicon4.png";
-          definedAliases = [ "@dictcc" ];
+          "dict.cc Dictionary" = {
+            urls = [ { template = "https://www.dict.cc/?s={searchTerms}"; } ];
+            icon = "https://www4.dict.cc/img/favicons/favicon4.png";
+            definedAliases = [ "@dictcc" ];
+          };
         };
       };
 
